87f6a4641fa4cca96863a7e7a5c3b66221d8c602
zhiyong <13852292775@163.com>
2018-08-08 16:45:06 +0800

    'xx'

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,18 +1,22 @@
 // pages/list/list.js
+let datas = require('../../datas/list-data.js');
+console.log(datas, typeof datas);
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+		listArr: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+		this.setData({
+			listArr: datas.list_data
+		})
   },
 
   /**
